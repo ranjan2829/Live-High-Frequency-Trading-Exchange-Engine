f8a4ed67b8c0e1d606ce814795c58b9d48f395de
Ranjan <73215366+ranjan2829@users.noreply.github.com>
2024-02-01 10:13:03 +0000

    Rust Trading Engine (SImple order matching)

--- Rust_Trading_Engine/src/main.rs
+++ Rust_Trading_Engine/src/main.rs
@@ -3,6 +3,30 @@ enum BidorAsk{
     Bid,
     Ask,
 }
+struct Price{
+    integral:u64,
+    fractional:u64,
+    scalar:u64,
+}
+impl Price{
+    fn new(price:f64)->Price{
+        let scalar=100000;
+        let integral=price as u64;
+        let fractional=((price % 1.0)*scalar as f64) as u64;
+        Price{
+            scalar,
+            integral,
+            fractional,
+        }
+    }
+
+
+}
+struct Limit{
+    price:Price,
+    orders:Vec<Order>,
+}
+
 
 struct Order{
     size: f64,
@@ -10,11 +34,11 @@ struct Order{
 
 
 }
-imple Order{
+impl Order{
     fn new(bid_or_ask:BidorAsk,size:f64)->Order{
         Order{bid_or_ask,size}
     }
 }
 fn main(){
-    
+
 }
