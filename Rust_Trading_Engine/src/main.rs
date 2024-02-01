8d804ca320d5a9099216ed37675109d1c3c80fe8
Ranjan <73215366+ranjan2829@users.noreply.github.com>
2024-02-01 09:55:39 +0000

    Rust Trading Engine (SImple order matching)

--- Rust_Trading_Engine/src/main.rs
+++ Rust_Trading_Engine/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+
+enum BidorAsk{
+    Bid,
+    Ask,
+}
+
+struct Order{
+    size: f64,
+    bid_or_ask:BidorAsk,
+
+
+}
+imple Order{
+    fn new(bid_or_ask:BidorAsk,size:f64)->Order{
+        Order{bid_or_ask,size}
+    }
+}
+fn main(){
+    
 }
