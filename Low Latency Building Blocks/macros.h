3d02649595803469bd1b0703029855797788e6b7
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-02-17 17:52:50 +0530

    macros created

--- Low Latency Building Blocks/macros.h
+++ Low Latency Building Blocks/macros.h
@@ -7,7 +7,13 @@
 inline auto ASSERT(bool cond ,const std::string &msg) noexcept{
     if(UNLIKELY(!cond)){
         std::cerr<<"ASSERT:"<<msg<<std::endl;
+        exit(EXIT_FAILURE);
 
     }
+
+}
+inline auto FATAL(const std::string &msg)noexcept{
+    std::cerr<<"FATAL :"<<msg<<std::endl;
+    exit(EXIT_FAILURE);
 
 }
