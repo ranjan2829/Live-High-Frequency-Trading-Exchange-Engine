6393b2d5efa0e2a16258819824e81c8c758e14d7
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-02-17 17:49:56 +0530

    macros created

---
+++ Low Latency Building Blocks/macros.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <cstring>
+#include <iostream>
+
+#define LIKELY(x) __builtin_expect(!!(x),1)
+#define UNLIKELY(x) __builtin_expect(!!(x),0)
+inline auto ASSERT(bool cond ,const std::string &msg) noexcept{
+    if(UNLIKELY(!cond)){
+        std::cerr<<"ASSERT:"<<msg<<std::endl;
+
+    }
+
+}
