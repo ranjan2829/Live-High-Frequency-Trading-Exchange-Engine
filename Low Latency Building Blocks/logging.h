517098d413f6da27d43817a2f99175667277e3ae
Ranjan <73215366+ranjan2829@users.noreply.github.com>
2024-02-29 10:42:10 +0000

    logging low latency

---
+++ Low Latency Building Blocks/logging.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <string>
+#include <fstream>
+#include <cstdio>
+#include "macros.h"
+#include "LockFreeQueues.h"
+#include "thread_utils.h"
+#include "time_utils.h"
+namespace Common{
+    constexpr size_t LOQ_QUEUE_SIZE=8*1024*1024;
+    enum class LogType : int8_t{
+        CHAR = 0,
+        INTEGER = 1,
+        LONG_INTEGER = 2,
+        LONG_LONG_INTEGER = 3,
+        UNSIGNED_INTEGER = 4,
+        UNSIGNED_LONG_INTEGER = 5,
+        UNSIGNED_LONG_LONG_INTEGER = 6,
+        FLOAT = 7,
+        DOUBLE = 8
+
+    };
+    struct LogElement{
+        LogType type_=LogType::CHAR;
+        
+    }
+
+    }
+
+}
