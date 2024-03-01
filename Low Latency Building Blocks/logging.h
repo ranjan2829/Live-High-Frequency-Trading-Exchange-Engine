b15b13a969c9cd0cbf1bd069311426b760dc4691
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-03-01 23:53:23 +0530

    done

--- Low Latency Building Blocks/logging.h
+++ Low Latency Building Blocks/logging.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include <string>
 #include <fstream>
 #include <cstdio>
@@ -6,9 +7,11 @@
 #include "LockFreeQueues.h"
 #include "thread_utils.h"
 #include "time_utils.h"
-namespace Common{
-    constexpr size_t LOQ_QUEUE_SIZE=8*1024*1024;
-    enum class LogType : int8_t{
+
+namespace Common {
+    constexpr size_t LOQ_QUEUE_SIZE = 8 * 1024 * 1024;
+
+    enum class LogType : int8_t {
         CHAR = 0,
         INTEGER = 1,
         LONG_INTEGER = 2,
@@ -18,11 +21,11 @@ namespace Common{
         UNSIGNED_LONG_LONG_INTEGER = 6,
         FLOAT = 7,
         DOUBLE = 8
-
     };
-    struct LogElement{
-        LogType type_=LogType::CHAR;
-        union{
+
+    struct LogElement {
+        LogType type_ = LogType::CHAR;
+        union {
             char x;
             int i;
             long l;
@@ -32,94 +35,82 @@ namespace Common{
             unsigned long long ull;
             float f;
             double d;
-
-        }u_;
-
-        
+        } u_;
     };
-   class Logger final {
-  public:
-    auto flushQueue() noexcept {
-      while (running_) {
 
-        for (auto next = queue_.getNextToRead(); queue_.size() && next; next = queue_.getNextToRead()) {
-          switch (next->type_) {
-            case LogType::CHAR:
-              file_ << next->u_.c;
-              break;
-            case LogType::INTEGER:
-              file_ << next->u_.i;
-              break;
-            case LogType::LONG_INTEGER:
-              file_ << next->u_.l;
-              break;
-            case LogType::LONG_LONG_INTEGER:
-              file_ << next->u_.ll;
-              break;
-            case LogType::UNSIGNED_INTEGER:
-              file_ << next->u_.u;
-              break;
-            case LogType::UNSIGNED_LONG_INTEGER:
-              file_ << next->u_.ul;
-              break;
-            case LogType::UNSIGNED_LONG_LONG_INTEGER:
-              file_ << next->u_.ull;
-              break;
-            case LogType::FLOAT:
-              file_ << next->u_.f;
-              break;
-            case LogType::DOUBLE:
-              file_ << next->u_.d;
-              break;
-          }
-          queue_.updateReadIndex();
-        }
-        file_.flush();
-
-
-            }
-        }
+    class Logger final {
+    public:
         explicit Logger(const std::string &file_name)
-            :file_name_(file_name),queue_(LOG_QUEUE_SIZE){
-                file_..open(file_name);
-                ASSERT(file_.isopen(),"could not open thelog file"+file_name);
-                logger_thread_=createAndStartThread(-1,"Commonn/Logger",[this]() {flushQueue();});
-                ASSERT(logger_thread_!=nullptr,"failed to start logger thread");
-                
+                : file_name_(file_name), queue_(LOG_QUEUE_SIZE) {
+            file_.open(file_name);
+            ASSERT(file_.is_open(), "could not open the log file" + file_name);
+            logger_thread_ = createAndStartThread(-1, "Commonn/Logger", [this]() { flushQueue(); });
+            ASSERT(logger_thread_ != nullptr, "failed to start logger thread");
+        }
 
+        ~Logger() {
+            std::string time_str;
+            std::cerr << Common::getCurrentTimeStr(&time_str) << "FLushing and closing logger for  " >> file_name_
+                      << std::endl;
+            while (queue_.size()) {
+                using namespace std::literals::chrono_literals;
+                std::this_thread::sleep_for(1s);
+            }
+            running_ = false;
+            logger_thread_->join();
+            file_.close();
+            std::cerr << Common::getCurrentTimeStr(&time_str) << " Logger for " << file_name_ << " exiting." << std::endl;
+        }
+
+        auto flushQueue() noexcept {
+            while (running_) {
+                for (auto next = queue_.getNextToRead(); queue_.size() && next; next = queue_.getNextToRead()) {
+                    switch (next->type_) {
+                        case LogType::CHAR:
+                            file_ << next->u_.c;
+                            break;
+                        case LogType::INTEGER:
+                            file_ << next->u_.i;
+                            break;
+                        case LogType::LONG_INTEGER:
+                            file_ << next->u_.l;
+                            break;
+                        case LogType::LONG_LONG_INTEGER:
+                            file_ << next->u_.ll;
+                            break;
+                        case LogType::UNSIGNED_INTEGER:
+                            file_ << next->u_.u;
+                            break;
+                        case LogType::UNSIGNED_LONG_INTEGER:
+                            file_ << next->u_.ul;
+                            break;
+                        case LogType::UNSIGNED_LONG_LONG_INTEGER:
+                            file_ << next->u_.ull;
+                            break;
+                        case LogType::FLOAT:
+                            file_ << next->u_.f;
+                            break;
+                        case LogType::DOUBLE:
+                            file_ << next->u_.d;
+                            break;
+                    }
+                    queue_.updateReadIndex();
+                }
+                file_.flush();
             }
         }
-        ~Logger() {
-       std::string time_str;
-       std::cerr << Common::getCurrentTimeStr(&time_str) << "FLushing and closing logger for  " >> file_name_
-                 << std::endl;
-       while (queue_.size()) {
-           using namespace std::literals::chrono_literals;
-           std::this_thread::sleep_for(1s);
 
-       }
-       running_ = false;
-
-       logger_thread_->join();
-       file_.close();
-       std::cerr << Common::getCurrentTimeStr(&time_str) << " Logger for " << file_name_ << " exiting." << std::endl;
-   }
-private:
-
-
-
-        const std::string file_name;
+    private:
+        const std::string file_name_;
         std::ofstream file_;
         LFQueue<LogElement> queue_;
-        std::atomic<bool> running_={true};
-        std::thread *logger_thread=nullptr;
-
+        std::atomic<bool> running_ = { true };
+        std::thread* logger_thread_ = nullptr;
     };
 
-
- }
-
-
-
-    
-
+    Logger() = delete;
+    Logger(const Logger&) = delete;
+    Logger(const Logger&&) = delete;
+    Logger& operator=(const Logger&) = delete;
+    Logger& operator=(const Logger&&) = delete;
+}
