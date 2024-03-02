302b964922442ac50df38a6f3772af92a1901688
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-03-02 12:17:07 +0530

    done

--- Low Latency Building Blocks/logging.h
+++ Low Latency Building Blocks/logging.h
@@ -3,13 +3,14 @@
 #include <string>
 #include <fstream>
 #include <cstdio>
+
 #include "macros.h"
-#include "LockFreeQueues.h"
+#include "lf_queue.h"
 #include "thread_utils.h"
 #include "time_utils.h"
 
 namespace Common {
-    constexpr size_t LOQ_QUEUE_SIZE = 8 * 1024 * 1024;
+    constexpr size_t LOG_QUEUE_SIZE = 8 * 1024 * 1024;
 
     enum class LogType : int8_t {
         CHAR = 0,
@@ -26,7 +27,7 @@ namespace Common {
     struct LogElement {
         LogType type_ = LogType::CHAR;
         union {
-            char x;
+            char c;
             int i;
             long l;
             long long ll;
@@ -40,30 +41,9 @@ namespace Common {
 
     class Logger final {
     public:
-        explicit Logger(const std::string &file_name)
-                : file_name_(file_name), queue_(LOG_QUEUE_SIZE) {
-            file_.open(file_name);
-            ASSERT(file_.is_open(), "could not open the log file" + file_name);
-            logger_thread_ = createAndStartThread(-1, "Commonn/Logger", [this]() { flushQueue(); });
-            ASSERT(logger_thread_ != nullptr, "failed to start logger thread");
-        }
-
-        ~Logger() {
-            std::string time_str;
-            std::cerr << Common::getCurrentTimeStr(&time_str) << "FLushing and closing logger for  " >> file_name_
-                      << std::endl;
-            while (queue_.size()) {
-                using namespace std::literals::chrono_literals;
-                std::this_thread::sleep_for(1s);
-            }
-            running_ = false;
-            logger_thread_->join();
-            file_.close();
-            std::cerr << Common::getCurrentTimeStr(&time_str) << " Logger for " << file_name_ << " exiting." << std::endl;
-        }
-
         auto flushQueue() noexcept {
             while (running_) {
+
                 for (auto next = queue_.getNextToRead(); queue_.size() && next; next = queue_.getNextToRead()) {
                     switch (next->type_) {
                         case LogType::CHAR:
@@ -97,67 +77,96 @@ namespace Common {
                     queue_.updateReadIndex();
                 }
                 file_.flush();
+
+                using namespace std::literals::chrono_literals;
+                std::this_thread::sleep_for(10ms);
             }
         }
-        auto pushValue(const LogElement &log_element) noexcept{
-            *(queue_.getNextToWriteTo())=log_element;
+
+        explicit Logger(const std::string &file_name)
+                : file_name_(file_name), queue_(LOG_QUEUE_SIZE) {
+            file_.open(file_name);
+            ASSERT(file_.is_open(), "Could not open log file:" + file_name);
+            logger_thread_ = createAndStartThread(-1, "Common/Logger " + file_name_, [this]() { flushQueue(); });
+            ASSERT(logger_thread_ != nullptr, "Failed to start Logger thread.");
+        }
+
+        ~Logger() {
+            std::string time_str;
+            std::cerr << Common::getCurrentTimeStr(&time_str) << " Flushing and closing Logger for " << file_name_ << std::endl;
+
+            while (queue_.size()) {
+                using namespace std::literals::chrono_literals;
+                std::this_thread::sleep_for(1s);
+            }
+            running_ = false;
+            logger_thread_->join();
+
+            file_.close();
+            std::cerr << Common::getCurrentTimeStr(&time_str) << " Logger for " << file_name_ << " exiting." << std::endl;
+        }
+
+        auto pushValue(const LogElement &log_element) noexcept {
+            *(queue_.getNextToWriteTo()) = log_element;
             queue_.updateWriteIndex();
-
-
         }
-        auto pushValue(cont int value) noexcept{
-            pushValue(LogElement{LogType::INTEGER,{.i=value}});
 
+        auto pushValue(const char value) noexcept {
+            pushValue(LogElement{LogType::CHAR, {.c = value}});
         }
-        auto pushValue(cont long value) noexcept{
-            pushValue(LogElement{LogType::LONG_INTEGER,{.l=value}});
 
+        auto pushValue(const int value) noexcept {
+            pushValue(LogElement{LogType::INTEGER, {.i = value}});
         }
-        auto pushValue(cont long long value) noexcept{
-            pushValue(LogElement{LogType::LONG_LONG_INTEGER,{.ll=value}});
 
+        auto pushValue(const long value) noexcept {
+            pushValue(LogElement{LogType::LONG_INTEGER, {.l = value}});
         }
-        auto pushValue(cont unsigned value) noexcept{
-            pushValue(LogElement{LogType:UNSIGNED_INTEGER,{.u=value}});
 
+        auto pushValue(const long long value) noexcept {
+            pushValue(LogElement{LogType::LONG_LONG_INTEGER, {.ll = value}});
         }
-        auto pushValue(cont unsigned long value) noexcept{
-            pushValue(LogElement{LogType::UNSIGNED_LONG_INTEGER,{.ul=value}});
 
+        auto pushValue(const unsigned value) noexcept {
+            pushValue(LogElement{LogType::UNSIGNED_INTEGER, {.u = value}});
         }
-        auto pushValue(cont unsigned long long value) noexcept{
-            pushValue(LogElement{LogType::UNSIGNED_LONG_LONG_INTEGER,{.ul=value}});
 
+        auto pushValue(const unsigned long value) noexcept {
+            pushValue(LogElement{LogType::UNSIGNED_LONG_INTEGER, {.ul = value}});
         }
-        auto pushValue(cont float value) noexcept{
-            pushValue(LogElement{LogType::FLOAT,{.f=value}});
 
+        auto pushValue(const unsigned long long value) noexcept {
+            pushValue(LogElement{LogType::UNSIGNED_LONG_LONG_INTEGER, {.ull = value}});
         }
-        auto pushValue(cont double value) noexcept{
-            pushValue(LogElement{LogType::DOUBLE,{.d=value}});
 
+        auto pushValue(const float value) noexcept {
+            pushValue(LogElement{LogType::FLOAT, {.f = value}});
         }
-        auto pushValue(cont char *value) noexcept{
-            while(*value){
+
+        auto pushValue(const double value) noexcept {
+            pushValue(LogElement{LogType::DOUBLE, {.d = value}});
+        }
+
+        auto pushValue(const char *value) noexcept {
+            while (*value) {
                 pushValue(*value);
                 ++value;
             }
-
         }
-        auto pushValue(cont std:: string &value) noexcept{
+
+        auto pushValue(const std::string &value) noexcept {
             pushValue(value.c_str());
-
         }
-        template<typename T,typename A>
-        auto log (const char *s,const T &value,A..args) noexcept{
-            while(*s){
-                if(*s=='%'){
-                    if(UNLIKELY(*(s+1)=='%')){
+
+        template<typename T, typename... A>
+        auto log(const char *s, const T &value, A... args) noexcept {
+            while (*s) {
+                if (*s == '%') {
+                    if (UNLIKELY(*(s + 1) == '%')) { // to allow %% -> % escape character.
                         ++s;
-                    }
-                    else{
-                        pushValue(value);
-                        log(s+1,args...);
+                    } else {
+                        pushValue(value); // substitute % with the value specified in the arguments.
+                        log(s + 1, args...); // pop an argument and call self recursively.
                         return;
                     }
                 }
@@ -166,18 +175,37 @@ namespace Common {
             FATAL("extra arguments provided to log()");
         }
 
+        // note that this is overloading not specialization. gcc does not allow inline specializations.
+        auto log(const char *s) noexcept {
+            while (*s) {
+                if (*s == '%') {
+                    if (UNLIKELY(*(s + 1) == '%')) { // to allow %% -> % escape character.
+                        ++s;
+                    } else {
+                        FATAL("missing arguments to log()");
+                    }
+                }
+                pushValue(*s++);
+            }
+        }
+
+        // Deleted default, copy & move constructors and assignment-operators.
         Logger() = delete;
-        Logger(const Logger&) = delete;
-        Logger(const Logger&&) = delete;
-        Logger& operator=(const Logger&) = delete;
-        Logger& operator=(const Logger&&) = delete;
+
+        Logger(const Logger &) = delete;
+
+        Logger(const Logger &&) = delete;
+
+        Logger &operator=(const Logger &) = delete;
+
+        Logger &operator=(const Logger &&) = delete;
 
     private:
         const std::string file_name_;
         std::ofstream file_;
-        LFQueue<LogElement> queue_;
-        std::atomic<bool> running_ = { true };
-        std::thread* logger_thread_ = nullptr;
-    };
 
+        LFQueue<LogElement> queue_;
+        std::atomic<bool> running_ = {true};
+        std::thread *logger_thread_ = nullptr;
+    };
 }
