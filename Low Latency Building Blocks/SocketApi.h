4bf1209e8c403253cca0ab9867c4889295dc4155
Ranjan <73215366+ranjan2829@users.noreply.github.com>
2024-03-06 06:08:19 +0000

    hft socket api codes

--- Low Latency Building Blocks/SocketApi.h
+++ Low Latency Building Blocks/SocketApi.h
@@ -14,3 +14,43 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <fcntl.h>
+
+
+#include "macros.h"
+#include "logging.h"
+namespace Common{
+    struct SocketCfg{
+        std::string ip_;
+        std::string iface_;
+        int port_=-1;
+        bool is_udp_=false;
+        bool is_listening_=false;
+        bool needs_so_timestamp_=false;
+        auto toString() const {
+            std::stringstream ss;
+            ss<<"SocketCfg[ip :"<<ip_
+            << "iface :"<<iface_
+            <<"port : "<<port_
+            <<"is udp :"<<is_udp_
+            <<"is listening  "<< is_listening_
+            <<"need s0 timestamp :"<<needs_so_timestamp_
+            <<"]";
+            return ss.str();
+        }
+
+
+    };
+    constexpr int MaxTcpServerBacklog =1024;
+    inline auto getIfaceIP(const std::string &iface)->std::string{
+        char buf[NI_MAXHOST]={'\0'};
+        ifaddrs *ifaddr =nullptr;
+        if(getifaddrs(&ifaddr)!=-1){
+            if(ifaddrs *ifa =ifaddr;ifa;ifa=ifa->ifa_next){
+                
+            }
+        }
+
+
+    }
+    
+}
