3f004d9bfad6801d0b0a2d1ab37244c1fa7e60c3
Ranjan <73215366+ranjan2829@users.noreply.github.com>
2024-06-11 16:00:32 +0000

    done

--- Exchange Matching Engine /Common Files/types.h
+++ Exchange Matching Engine /Common Files/types.h
@@ -2,3 +2,30 @@
 #include <cstdint>
 #include <limits>
 #include "Common Files/macros.h"
+
+namespace Common{
+    typedef uint64_t OrderId;
+    constexpr auto OrderId_Invalid=std::numeric_limits<OrderId>>::max();
+    inline auto orderToString(OrderId order)->std::string{
+        if(UNLIKELY(order==OrderId_Invalid)){
+            return "INVALID";
+        }
+        return std::to_string(order);
+    }
+    typedef uint32_t TickerID;
+    constexpr auto TickerID_Invalid=std::numeric_limits<<TickerID>>::max();
+    inline auto TickerToString(TickerID ticker)->std::string{
+        if(UNLIKELY(ticker==TickerID_Invalid)){
+            return "IVALID";
+        }
+        return std::to_string(ticker);
+    }
+    typedef uint32_t ClientID;
+    constexpr auto ClientID_Invalid=std::numeric_limits<<ClientID>>::max();
+    inline auto ClientToString(ClientID client)->std::string{
+        if(UNLIKELY(clientr==ClientID_Invalid)){
+            return "IVALID";
+        }
+        return std::to_string(client);
+    }
+}
