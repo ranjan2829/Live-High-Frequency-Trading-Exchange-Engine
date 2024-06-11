3c9d548f44b4ab0f52440af925bac0a5bcfab840
Ranjan <73215366+ranjan2829@users.noreply.github.com>
2024-06-11 15:43:12 +0000

    f

--- Exchange Matching Engine /Common Files/types.h
+++ Exchange Matching Engine /Common Files/types.h
@@ -2,14 +2,3 @@
 #include <cstdint>
 #include <limits>
 #include "Common Files/macros.h"
-namespace Common{
-    typedef uint64_t orderId;
-    contexpr auto orderId_invalid=std::numeric_limits<orderId>>::max();
-    inline auto orderIdToString(orderId order)->std::string{
-        if(UNLIKELY(order == orderId_invalid)){
-            return "INVALID";
-        }
-        return std::to_string(order);
-    }
-
-}
