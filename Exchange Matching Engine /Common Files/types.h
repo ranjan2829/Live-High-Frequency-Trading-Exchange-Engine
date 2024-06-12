7763b88231320e93631e38c74f89ea383e762905
Ranjan <73215366+ranjan2829@users.noreply.github.com>
2024-06-12 10:42:48 +0000

    done

--- Exchange Matching Engine /Common Files/types.h
+++ Exchange Matching Engine /Common Files/types.h
@@ -4,6 +4,15 @@
 #include "Common Files/macros.h"
 
 namespace Common{
+    constexpr side_t LOG_QUEUE_SIZE=8*1024*1024;
+    constexpr size_t ME_MAX_TICKERS=8;
+    constexpr size_t ME_MAX_CLIENT_UPDATES=256*1024;
+    constexpr size_t ME_MAX_MARKET_UPDATES=256*1024;
+    constexpr size_t ME_MAX_NUM_CLIENTS=256;
+    constexpr size_t ME_MAX_ORDER_IDS =1024*1024;
+    constexpr size_t ME_MAX_PRICE_LEVELS=256;
+    
+
     typedef uint64_t OrderId;
     constexpr auto OrderId_Invalid=std::numeric_limits<OrderId>>::max();
     inline auto orderToString(OrderId order)->std::string{
