28c44d69a7df9e0c40de03031e78e2bf94a76e14
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-06-13 00:57:23 +0530

    done

--- Exchange Matching Engine /EXCHANGE/Order-Server/client_request.h
+++ Exchange Matching Engine /EXCHANGE/Order-Server/client_request.h
@@ -25,22 +25,35 @@ namespace Exchange {
         }
         return "UNKNOWN";
     }
-    struct MEClientRequest{
-        ClientRequestType type_=ClientRequestType::INVALID;
-        ClientId client_id =ClientId_INVALID;
-        TickerId tickerId =TickerId_INVALID;
-        OrderId orderId =OrderId_INVALID;
-        Side side=Side::INVALID;
-        Price price_=Price_INVALID;
-        Qty qty_=Qty_INVALID;
-        autp toString() const {
+
+    struct MEClientRequest {
+        ClientRequestType type_ = ClientRequestType::INVALID;
+
+        ClientId client_id_ = ClientId_INVALID;
+        TickerId ticker_id_ = TickerId_INVALID;
+        OrderId order_id_ = OrderId_INVALID;
+        Side side_ = Side::INVALID;
+        Price price_ = Price_INVALID;
+        Qty qty_ = Qty_INVALID;
+
+        auto toString() const {
             std::stringstream ss;
-
+            ss << "MEClientRequest"
+               << " ["
+               << "type:" << clientRequestTypeToString(type_)
+               << " client:" << clientIdToString(client_id_)
+               << " ticker:" << tickerIdToString(ticker_id_)
+               << " oid:" << orderIdToString(order_id_)
+               << " side:" << sideToString(side_)
+               << " qty:" << qtyToString(qty_)
+               << " price:" << priceToString(price_)
+               << "]";
+            return ss.str();
         }
-
-
     };
+
+#pragma pack(pop)
+
+    typedef LFQueue<MEClientRequest> ClientRequestLFQueue;
 }
-
-
 
