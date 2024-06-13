ac3fd34ea809bd9642aecb8ef4365b4844fd6b48
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-06-13 13:24:29 +0530

    done

--- Exchange Matching Engine /EXCHANGE/MatchingEngine/ME_order.h
+++ Exchange Matching Engine /EXCHANGE/MatchingEngine/ME_order.h
@@ -2,7 +2,29 @@
 // Created by Ranjan shahaji shitole on 13/06/24.
 //
 
-#ifndef HFT_DEVELOPER_CODES_ME_ORDER_H
-#define HFT_DEVELOPER_CODES_ME_ORDER_H
+#pragma once
+#include <array>
+#include <sstream>
+#include "Exchange Matching Engine /Common Files/types.h"
 
-#endif //HFT_DEVELOPER_CODES_ME_ORDER_H
+using namespace Common;
+
+namespace Exchange{
+    struct MEOrder{
+        TickerId ticker_Id_=TickerId_INVALID;
+        ClientId client_id_=ClientId_INVALID;
+        OrderId client_order_id_=OrderId_INVALID;
+        OrderId market_order_id_=OrderId_INVALID;
+        Side side_=Side::INVALID;
+        Price price_=Price_INVALID;
+        Qty qty_=Qty_INVALID;
+        Priority priority_ =Priority_INVALID;
+        MEOrder *prev_order_=nullptr;
+        MEOrder *nest_order_=nullptr;
+
+
+        MEOrder()=default;
+
+
+    };
+};
