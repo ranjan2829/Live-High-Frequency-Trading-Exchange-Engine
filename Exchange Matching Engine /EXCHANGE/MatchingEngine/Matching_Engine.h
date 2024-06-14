6d07c53f3d10d5fb2d738dfe1b8d2edc784fb709
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-06-14 11:32:44 +0530

    done

--- Exchange Matching Engine /EXCHANGE/MatchingEngine/Matching_Engine.h
+++ Exchange Matching Engine /EXCHANGE/MatchingEngine/Matching_Engine.h
@@ -11,3 +11,34 @@
 #include "Exchange Matching Engine /EXCHANGE/Market-Update/market_update.h"
 #include "me_order_book.h"
 
+namespace Exchange{
+    class MatchingEngine final {
+    public:
+        MatchingEngine(ClientRequestLFQueue *client_requests,
+                       ClientResponseLFQueue *client_response,
+                       MEMarketUpdateLFQueue *market_updates);
+        ~MatchingEngine();
+        auto start() ->void;
+        auto stop() ->void;
+
+
+
+        MatchingEngine() =delete;
+        MatchingEngine(const MatchEngine &)=delete;
+        MatchtingEngine &operator =(const MatchingEngine &)=delete;
+        MatchingEngine &operator =(const MatchingEngine &&)=delete;
+    private:
+        OrderBookHashMap ticker_order_;
+        ClientRequestLFQueue *incoming_requests_=nullptr;
+        ClientResponeLFQueue *outgoing_ogw_responses_=nullptr;
+        MEMarketUpdateLFQueue *outgoing_md_updates_=nullptr;
+
+        volatile vool run_=false;
+
+        std::string time_str;
+        Logger logger_;
+
+
+
+    };
+}
--- Exchange Matching Engine /EXCHANGE/MatchingEngine/me_order_book.h
+++ Exchange Matching Engine /EXCHANGE/MatchingEngine/me_order_book.h
@@ -2,7 +2,3 @@
 // Created by Ranjan shahaji shitole on 13/06/24.
 //
 
-#ifndef HFT_DEVELOPER_CODES_ME_ORDER_BOOK_H
-#define HFT_DEVELOPER_CODES_ME_ORDER_BOOK_H
-
-#endif //HFT_DEVELOPER_CODES_ME_ORDER_BOOK_H
